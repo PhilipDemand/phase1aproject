5125ad2e04eb1af1928708d7002567012262ed34
Philip Demand <philipdemand@yahoo.com>
2023-02-09 13:58:28 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+fetchAndRenderTools()
+
+function fetchAndRenderTools() {
+    fetch("http://localhost:3000/tools")
+        .then(response => response.json())
+        .then( (someData) => {
+            const arrayOfToolObjects = someData
+            const toolCollectionDiv = document.querySelector("#tool-menu")
+            arrayOfToolObjects.map(  
+                (eachToolObject)=>{
+                  const toolImage = document.createElement("img")
+                    toolImage.src = eachToolObject.image
+                    toolImage.id = eachToolObject.id
+                  toolImage.addEventListener("click", function(event) {
+                    const clickedImage = event.target;
+                    const bigImage = document.getElementsByClassName("detail-image");
+                    bigImage[0].src = clickedImage.src
+                    // bigImage[0].id = eachToolObject.id
+                    const bigName = document.getElementsByClassName("name");
+                    bigName[0].innerText = eachToolObject.name
+                    const toolAvailability = document.getElementsByClassName("status");
+                    toolAvailability[0].innerText = eachToolObject.availability
+                  })
+                  toolCollectionDiv.append(toolImage)
+              })
+            const centerImage = document.getElementById("center-image")
+            // centerImage.addEventListener("mouseover", function() {
+            //   const imgId = centerImage.id
+            //   const adjustedImgId = imgId - 1
+            //   const description = arrayOfToolObjects[adjustedImgId].useDescription
+            // })
+
+            // centerImage.addEventListener("mouseout", function() {
+            //   //console.log("goodbye")
+            // })
+
+            })
+            }
+
+const centerImageDiv = document.getElementById("tool-detail")
+
+const submitForm = document.getElementById("new-tool");  
+  submitForm.addEventListener("submit", function(e) {
+   e.preventDefault()
+   const submittedObject = {
+    name:e.target[0].value, 
+    availability:"Available",
+    phonenumber:e.target[1].value,
+    image:e.target[2].value,
+    useDescription:e.target[3].value
+  }
+
+  fetch("http://localhost:3000/tools", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", 
+        Accept: "application/json"
+      },
+      body: JSON.stringify(submittedObject)
+    })
+    .then(response => response.json())
+    .then( () => fetchAndRenderTools())
+  })
